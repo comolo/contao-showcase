5c3d9eaa69110a27e0cba91e81703a42669c97dd
Hendrik Obermayer <h.obermayer@comolo.de>
2018-07-26 15:40:46 +0200

    created basic dca structure

--- src/Resources/contao/dca/tl_showcase_category.php
+++ src/Resources/contao/dca/tl_showcase_category.php
@@ -1,2 +1,140 @@
 <?php
+/*
+ * This file is part of the Showcase extension by Comolo.
+ *
+ * Copyright (C) 2018 Comolo GmbH
+ *
+ * @author    Hendrik Obermayer <https://github.com/henobi>
+ * @copyright 2018 Comolo GmbH <https://www.comolo.de/>
+ * @license   proprietary
+ */
+
+
+/**
+ * Table tl_showcase_folder
+ */
+$GLOBALS['TL_DCA']['tl_showcase_category'] = array
+(
+
+    // Config
+    'config' => array
+    (
+        'dataContainer'               => 'Table',
+        'ptable'                      => 'tl_showcase_folder',
+        'enableVersioning'            => true,
+        'sql' => array
+        (
+            'keys' => array
+            (
+                'id' => 'primary'
+            )
+        ),
+    ),
+
+    // List
+    'list' => array
+    (
+        'sorting' => array
+        (
+            'mode'                    => 1,
+            'fields'                  => array('title'),
+            'flag'                    => 1
+        ),
+        'label' => array
+        (
+            'fields'                  => array('title'),
+            'format'                  => '%s'
+        ),
+        'global_operations' => array
+        (
+            'all' => array
+            (
+                'label'               => &$GLOBALS['TL_LANG']['MSC']['all'],
+                'href'                => 'act=select',
+                'class'               => 'header_edit_all',
+                'attributes'          => 'onclick="Backend.getScrollOffset();" accesskey="e"'
+            )
+        ),
+        'operations' => array
+        (
+            'edit' => array
+            (
+                'label'               => &$GLOBALS['TL_LANG']['tl_showcase_category']['edit'],
+                'href'                => 'act=edit',
+                'icon'                => 'edit.svg',
+            ),
+            'documents' => array
+            (
+                'label'               => &$GLOBALS['TL_LANG']['tl_showcase_category']['documents'],
+                'href'                => 'table=tl_immoverwaltung_document',
+                'icon'                => 'folderC.svg'
+            ),
+            'copy' => array
+            (
+                'label'               => &$GLOBALS['TL_LANG']['tl_showcase_category']['copy'],
+                'href'                => 'act=copy',
+                'icon'                => 'copy.gif'
+            ),
+            'delete' => array
+            (
+                'label'               => &$GLOBALS['TL_LANG']['tl_showcase_category']['delete'],
+                'href'                => 'act=delete',
+                'icon'                => 'delete.gif',
+                'attributes'          => 'onclick="if(!confirm(\'' . $GLOBALS['TL_LANG']['MSC']['deleteConfirm'] . '\'))return false;Backend.getScrollOffset()"'
+            ),
+            'show' => array
+            (
+                'label'               => &$GLOBALS['TL_LANG']['tl_showcase_category']['show'],
+                'href'                => 'act=show',
+                'icon'                => 'show.gif'
+            )
+        )
+    ),
+
+    // Select
+    'select' => array
+    (
+        'buttons_callback' => array()
+    ),
+
+    // Edit
+    'edit' => array
+    (
+        'buttons_callback' => array()
+    ),
+
+    // Palettes
+    'palettes' => array
+    (
+        '__selector__'                => array(''),
+        'default'                     => '{general_legend},title;'
+    ),
+
+    // Subpalettes
+    'subpalettes' => array
+    (
+        ''                            => ''
+    ),
+
+    // Fields
+    'fields' => array
+    (
+        'id' => array
+        (
+            'sql'                     => "int(10) unsigned NOT NULL auto_increment"
+        ),
+        'tstamp' => array
+        (
+            'sql'                     => "int(10) unsigned NOT NULL default '0'"
+        ),
+        'title' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_showcase_category']['title'],
+            'exclude'                 => true,
+            'inputType'               => 'text',
+            'eval'                    => array('mandatory'=>true, 'maxlength'=>255, 'tl_class'=>'w50'),
+            'sql'                     => "varchar(255) NOT NULL default ''"
+        ),
+    )
+);
 
--- src/Resources/contao/dca/tl_showcase_folder.php
+++ src/Resources/contao/dca/tl_showcase_folder.php
@@ -1,2 +1,146 @@
 <?php
+/*
+ * This file is part of the Showcase extension by Comolo.
+ *
+ * Copyright (C) 2018 Comolo GmbH
+ *
+ * @author    Hendrik Obermayer <https://github.com/henobi>
+ * @copyright 2018 Comolo GmbH <https://www.comolo.de/>
+ * @license   proprietary
+ */
+
+
+/**
+ * Table tl_showcase_folder
+ */
+$GLOBALS['TL_DCA']['tl_showcase_folder'] = array
+(
+
+    // Config
+    'config' => array
+    (
+        'dataContainer'               => 'Table',
+        'ctable'                      => ['tl_showcase_entry', 'tl_showcase_category'],
+        'enableVersioning'            => true,
+        'sql' => array
+        (
+            'keys' => array
+            (
+                'id' => 'primary'
+            )
+        ),
+    ),
+
+    // List
+    'list' => array
+    (
+        'sorting' => array
+        (
+            'mode'                    => 1,
+            'fields'                  => array('title'),
+            'flag'                    => 1
+        ),
+        'label' => array
+        (
+            'fields'                  => array('title'),
+            'format'                  => '%s'
+        ),
+        'global_operations' => array
+        (
+            'all' => array
+            (
+                'label'               => &$GLOBALS['TL_LANG']['MSC']['all'],
+                'href'                => 'act=select',
+                'class'               => 'header_edit_all',
+                'attributes'          => 'onclick="Backend.getScrollOffset();" accesskey="e"'
+            )
+        ),
+        'operations' => array
+        (
+            'edit' => array
+            (
+                'label'               => &$GLOBALS['TL_LANG']['tl_showcase_folder']['edit'],
+                'href'                => 'act=edit',
+                'icon'                => 'edit.svg',
+            ),
+            'entries' => array
+            (
+                'label'               => &$GLOBALS['TL_LANG']['tl_showcase_folder']['entries'],
+                'href'                => 'table=tl_showcase_entry',
+                'icon'                => 'folderC.svg'
+            ),
+            'categories' => array
+            (
+                'label'               => &$GLOBALS['TL_LANG']['tl_showcase_folder']['categories'],
+                'href'                => 'table=tl_showcase_category',
+                'icon'                => 'folderB.svg'
+            ),
+            'copy' => array
+            (
+                'label'               => &$GLOBALS['TL_LANG']['tl_showcase_folder']['copy'],
+                'href'                => 'act=copy',
+                'icon'                => 'copy.gif'
+            ),
+            'delete' => array
+            (
+                'label'               => &$GLOBALS['TL_LANG']['tl_showcase_folder']['delete'],
+                'href'                => 'act=delete',
+                'icon'                => 'delete.gif',
+                'attributes'          => 'onclick="if(!confirm(\'' . $GLOBALS['TL_LANG']['MSC']['deleteConfirm'] . '\'))return false;Backend.getScrollOffset()"'
+            ),
+            'show' => array
+            (
+                'label'               => &$GLOBALS['TL_LANG']['tl_showcase_folder']['show'],
+                'href'                => 'act=show',
+                'icon'                => 'show.gif'
+            )
+        )
+    ),
+
+    // Select
+    'select' => array
+    (
+        'buttons_callback' => array()
+    ),
+
+    // Edit
+    'edit' => array
+    (
+        'buttons_callback' => array()
+    ),
+
+    // Palettes
+    'palettes' => array
+    (
+        '__selector__'                => array(''),
+        'default'                     => '{general_legend},title;'
+    ),
+
+    // Subpalettes
+    'subpalettes' => array
+    (
+        ''                            => ''
+    ),
+
+    // Fields
+    'fields' => array
+    (
+        'id' => array
+        (
+            'sql'                     => "int(10) unsigned NOT NULL auto_increment"
+        ),
+        'tstamp' => array
+        (
+            'sql'                     => "int(10) unsigned NOT NULL default '0'"
+        ),
+        'title' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_showcase_folder']['title'],
+            'exclude'                 => true,
+            'inputType'               => 'text',
+            'eval'                    => array('mandatory'=>true, 'maxlength'=>255, 'tl_class'=>'w50'),
+            'sql'                     => "varchar(255) NOT NULL default ''"
+        ),
+    )
+);
 
