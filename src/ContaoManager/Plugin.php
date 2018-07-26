4f856aa3f815563c30863787813d74d939d23694
Hendrik Obermayer <h.obermayer@comolo.de>
2018-07-26 15:28:42 +0200

    Created Contao Manager Class

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -1,15 +1,37 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: hendrik
- * Date: 26.07.18
- * Time: 14:56
+/*
+ * This file is part of the SuperTheme extension by Comolo.
+ *
+ * Copyright (C) 2018 Comolo GmbH
+ *
+ * @author    Hendrik Obermayer <https://github.com/henobi>
+ * @copyright 2018 Comolo GmbH <https://www.comolo.de/>
+ * @license   proprietary
  */
-
 namespace Comolo\ShowcaseBundle\ContaoManager;
 
+use Contao\CoreBundle\ContaoCoreBundle;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Comolo\ShowcaseBundle\ComoloShowcaseBundle;
 
-class Plugin
+/**
+ * Plugin for the Contao Manager.
+ *
+ * @author Hendrik Obermayer <https://github.com/henobi>
+ */
+class Plugin implements BundlePluginInterface
 {
-
+    /**
+     * {@inheritdoc}
+     */
+    public function getBundles(ParserInterface $parser)
+    {
+        return [
+            BundleConfig::create(ComoloShowcaseBundle::class)
+                ->setLoadAfter([ContaoCoreBundle::class])
+                ->setReplace(['showcase']),
+        ];
+    }
 }
