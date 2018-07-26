5d402709ba895d9f160d391f2551e4382cb3c308
Hendrik Obermayer <h.obermayer@comolo.de>
2018-07-26 16:11:15 +0200

    added translation

---
+++ src/Resources/contao/languages/de/modules.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ * This file is part of the Showcase extension by Comolo.
+ *
+ * Copyright (C) 2018 Comolo GmbH
+ *
+ * @author    Hendrik Obermayer <https://github.com/henobi>
+ * @copyright 2018 Comolo GmbH <https://www.comolo.de/>
+ * @license   proprietary
+ */
+
+
+/**
+ * Back end modules
+ */
+$GLOBALS['TL_LANG']['MOD']['showcase_folder'] = array('Showcase', 'Showcases anlegen und verwalten');
+
+
+/**
+ * Front end modules
+ */
+#$GLOBALS['TL_LANG']['FMD'][''] = array('', '');
