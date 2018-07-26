55340c4b3cf378467a45647c0098d0b40d7318f7
Hendrik Obermayer <h.obermayer@comolo.de>
2018-07-26 17:43:08 +0200

    added copyright

--- src/DcaTable/TlShowcaseEntry.php
+++ src/DcaTable/TlShowcaseEntry.php
@@ -1,11 +1,13 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: hendrik
- * Date: 26.07.18
- * Time: 17:09
+/*
+ * This file is part of the Showcase extension by Comolo.
+ *
+ * Copyright (C) 2018 Comolo GmbH
+ *
+ * @author    Hendrik Obermayer <https://github.com/henobi>
+ * @copyright 2018 Comolo GmbH <https://www.comolo.de/>
+ * @license   proprietary
  */
-
 namespace Comolo\ShowcaseBundle\DcaTable;
 
 /**
