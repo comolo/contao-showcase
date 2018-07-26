9cb1da1e00e4b582949749148d37de6263d8392e
Hendrik Obermayer <h.obermayer@comolo.de>
2018-07-26 17:23:29 +0200

    fixed translation

--- src/Resources/contao/languages/de/tl_showcase_entry.php
+++ src/Resources/contao/languages/de/tl_showcase_entry.php
@@ -36,10 +36,9 @@ $GLOBALS['TL_LANG']['tl_showcase_entry']['title'] = [
 /*
  * Showcase types
  */
-$GLOBALS['TL_LANG']['tl_showcase_entry']['types'] = [
-    'regular' => '',
-    'subpage' => '',
-];
+$GLOBALS['TL_LANG']['tl_showcase_entry']['types'] = [];
+$GLOBALS['TL_LANG']['tl_showcase_entry']['types']['regular'] = ['Standard', 'Darstellung der Referenz in der Lightbox'];
+$GLOBALS['TL_LANG']['tl_showcase_entry']['types']['subpage'] = ['Unterseite', 'Darstellung der Referenz auf einer Unterseite'];
 
 $GLOBALS['TL_LANG']['tl_showcase_entry']['type'] = ['Elementtyp', 'Bitte wählen Sie den Typ des Showcase Elements'];
 
