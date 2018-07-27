1354501b20aab3f4a7f6bf869f8fa57fc629fd4b
Hendrik Obermayer <h.obermayer@comolo.de>
2018-07-27 15:20:09 +0200

    Added translation

--- src/Resources/contao/languages/de/tl_showcase_entry.php
+++ src/Resources/contao/languages/de/tl_showcase_entry.php
@@ -1,6 +1,11 @@
 <?php
 
 $GLOBALS['TL_LANG']['tl_showcase_entry']['general_legend'] = 'Allgemein';
+$GLOBALS['TL_LANG']['tl_showcase_entry']['expert_legend'] = 'Experten-Einstellungen';
+$GLOBALS['TL_LANG']['tl_showcase_entry']['publish_legend'] = 'Veröffentlichung';
+$GLOBALS['TL_LANG']['tl_showcase_entry']['content_legend'] = 'Inhalt';
+$GLOBALS['TL_LANG']['tl_showcase_entry']['content_legend'] = 'Inhalt';
+$GLOBALS['TL_LANG']['tl_showcase_entry']['subheading_legend'] = 'Unterüberschrift';
 
 $GLOBALS['TL_LANG']['tl_showcase_entry']['new'] = [
     'Neues Showcase',
@@ -39,6 +44,16 @@ $GLOBALS['TL_LANG']['tl_showcase_entry']['title'] = [
 $GLOBALS['TL_LANG']['tl_showcase_entry']['types'] = [];
 $GLOBALS['TL_LANG']['tl_showcase_entry']['types']['regular'] = ['Kacheldarstellung mit Lightbox', 'Darstellung der Referenz in der Lightbox'];
 $GLOBALS['TL_LANG']['tl_showcase_entry']['types']['subpage'] = ['Kacheldarstellung mit Unterseite', 'Darstellung der Referenz auf einer Unterseite'];
-
 $GLOBALS['TL_LANG']['tl_showcase_entry']['type'] = ['Elementtyp', 'Bitte wählen Sie den Typ des Showcase Elements'];
 
+
+$GLOBALS['TL_LANG']['tl_showcase_entry']['published'] = ['Beitrag veröffentlichen', 'Den Beitrag auf der Webseite anzeigen.'];
+$GLOBALS['TL_LANG']['tl_showcase_entry']['start'] = ['Anzeigen ab', 'Den Beitrag erst ab diesem Tag auf der Webseite anzeigen.'];
+$GLOBALS['TL_LANG']['tl_showcase_entry']['stop'] = ['Anzeigen bis', 'Den Beitrag nur bis zu diesem Tag auf der Webseite anzeigen.'];
+$GLOBALS['TL_LANG']['tl_showcase_entry']['cssClass'] = ['CSS-Klasse', 'Hier können Sie eine oder mehrere Klassen eingeben.'];
+$GLOBALS['TL_LANG']['tl_showcase_entry']['subheadline'] = ['Unterüberschrift', 'Hier können Sie eine Unterüberschrift festlegen.'];
+$GLOBALS['TL_LANG']['tl_showcase_entry']['thumbnail'] = ['Thumbnail-Bild', 'Hier können Sie das Thumbnail-Bild festlegen, welches in der Übersicht angezeigt wird.'];
+$GLOBALS['TL_LANG']['tl_showcase_entry']['alias'] = ['Alias', 'Hier können Sie den Alias des Beitrags festlegen.'];
+$GLOBALS['TL_LANG']['tl_showcase_entry']['publishingDate'] = ['Datum', 'Hier können Sie eintragen, wann das Projekt umgesetzt wurde.'];
+$GLOBALS['TL_LANG']['tl_showcase_entry']['addUrl'] = ['Url / Youtube verlinken', 'Hier können Sie eine Url festlegen.'];
+$GLOBALS['TL_LANG']['tl_showcase_entry']['categories'] = ['Kategorien', 'Hier können Sie die Kategorien der Showcase festlegen.'];
